39ccc431fb91b9564710318112c762c31699a987
1000hyehyang <ducogus12@gmail.com>
2025-10-07 02:51:32 +0900

    feat/chapter4: Global 공통 엔티티 및 열거형 추가

---
+++ src/main/java/com/spring_b/thousandhyehyang/global/enums/NotificationType.java
@@ -0,0 +1,5 @@
+package com.spring_b.thousandhyehyang.global.enums;
+
+public enum NotificationType {
+    NEW_MISSION, REVIEW_REQUEST, SYSTEM
+}
---
+++ src/main/java/com/spring_b/thousandhyehyang/global/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.spring_b.thousandhyehyang.global.enums;
+
+public enum UserRole {
+    USER, OWNER, ADMIN
+}
