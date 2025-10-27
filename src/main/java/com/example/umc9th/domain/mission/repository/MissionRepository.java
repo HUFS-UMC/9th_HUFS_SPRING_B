3e1ef22dd4a5170f587d164594c475d51c8cd841
joby <rndlsrb7@gmail.com>
2025-10-27 20:36:22 +0900

    feat: 홈 화면 미션 목록

--- src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
@@ -1,4 +1,21 @@
 package com.example.umc9th.domain.mission.repository;
 
-public class MissionRepository {
+import com.example.umc9th.domain.mission.dto.MissionListResponse;
+import com.example.umc9th.domain.mission.entity.Mission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    @Query("SELECT new com.example.umc9th.domain.mission.dto.MissionListResponse(" +
+            "m.id, m.title, m.rewardPoint, s.name, s.address) " +
+            "FROM Mission m " +
+            "JOIN m.store s " +
+            "JOIN s.region r " +
+            "WHERE r.name = :regionName " +
+            "ORDER BY m.createdAt DESC")
+    Page<MissionListResponse> findMissionsByRegion(@Param("regionName") String regionName, Pageable pageable);
 }
