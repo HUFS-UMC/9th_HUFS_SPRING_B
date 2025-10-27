f4ac1bef9a8e2be23df6acbff49f3791796e7c8a
joby <rndlsrb7@gmail.com>
2025-10-27 21:05:38 +0900

    refactor: JPQL 쿼리와 일치하도록 엔티티 필드명 수정 및 매핑 정리

--- src/main/java/com/example/umc9th/domain/mission/repository/UserMissionRepository.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/UserMissionRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
 
     @Query("SELECT new com.example.umc9th.domain.mission.dto.UserMissionResponse(" +
-            "um.price, um.point, s.name) " +
+            "m.rewardPoint, s.name, um.status) " +
             "FROM UserMission um " +
             "JOIN um.mission m " +
             "JOIN m.store s " +
