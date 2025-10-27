033750957b7aeaa08a755608da6dafea6543c558
joby <rndlsrb7@gmail.com>
2025-10-27 20:34:20 +0900

    feat: 내가 진행/완료한 미션 조회

--- src/main/java/com/example/umc9th/domain/mission/dto/UserMissionResponse.java
+++ src/main/java/com/example/umc9th/domain/mission/dto/UserMissionResponse.java
@@ -1,4 +1,12 @@
 package com.example.umc9th.domain.mission.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserMissionResponse {
+    private Integer price;
+    private Integer point;
+    private String storeName;
 }
--- src/main/java/com/example/umc9th/domain/mission/repository/UserMissionRepository.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/UserMissionRepository.java
@@ -1,4 +1,21 @@
 package com.example.umc9th.domain.mission.repository;
 
-public class UserMissionRepository {
+import com.example.umc9th.domain.mission.dto.UserMissionResponse;
+import com.example.umc9th.domain.user.entity.mapping.UserMission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
+
+    @Query("SELECT new com.example.umc9th.domain.mission.dto.UserMissionResponse(" +
+            "um.price, um.point, s.name) " +
+            "FROM UserMission um " +
+            "JOIN um.mission m " +
+            "JOIN m.store s " +
+            "WHERE um.user.id = :userId " +
+            "ORDER BY um.mission.id DESC")
+    Page<UserMissionResponse> findUserMissions(@Param("userId") Long userId, Pageable pageable);
 }
