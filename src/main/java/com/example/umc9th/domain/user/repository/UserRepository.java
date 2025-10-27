6f15118c34f545e782b39ae5e19df2bac73a6cce
joby <rndlsrb7@gmail.com>
2025-10-27 20:28:23 +0900

    feat: 마이페이지(user + point_history JOIN)

--- src/main/java/com/example/umc9th/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/umc9th/domain/user/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.example.umc9th.domain.user.repository;
 
-public class UserRepository {
+import com.example.umc9th.domain.user.dto.UserPointResponse;
+import com.example.umc9th.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT new com.example.umc9th.domain.user.dto.UserPointResponse(" +
+            "u.name, u.email, u.phoneNumber, COALESCE(SUM(ph.amountPoint), 0)) " +
+            "FROM User u LEFT JOIN PointHistory ph ON u.id = ph.user.id " +
+            "WHERE u.id = :userId GROUP BY u.id")
+    UserPointResponse findUserPointInfo(@Param("userId") Long userId);
 }
