994b57752c0d2127761731c797271be131e8bc6b
kooinkyu <rndlsrb7@gmail.com>
2025-11-12 13:00:14 +0900

    feat:review api 응답통일

--- src/main/java/com/example/umc9th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc9th/domain/review/controller/ReviewController.java
@@ -2,6 +2,8 @@ package com.example.umc9th.domain.review.controller;
 
 import com.example.umc9th.domain.review.dto.ReviewResponse;
 import com.example.umc9th.domain.review.service.ReviewQueryService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +17,12 @@ public class ReviewController {
     private final ReviewQueryService reviewQueryService;
 
     @GetMapping("/search")
-    public List<ReviewResponse> searchMyReviews(
+    public ApiResponse<List<ReviewResponse>> searchMyReviews(
             @RequestParam Long userId,
             @RequestParam(required = false) String storeName,
             @RequestParam(required = false) Double rating
     ) {
-        return reviewQueryService.getMyReviews(userId, storeName, rating);
+        List<ReviewResponse> reviews = reviewQueryService.getMyReviews(userId, storeName, rating);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, reviews);
     }
 }
